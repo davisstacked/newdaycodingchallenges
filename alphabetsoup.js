945dc875b0d85dba586ec7e6ce9c3fdccf1bb4e6
davisstacked <brettzvi@gmail.com>
2021-10-28 10:41:27 -0700

    alphabet soup

---
+++ alphabetsoup.js
@@ -0,0 +1,16 @@
+// Alphabet Soup
+// Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
+// Examples
+// Input: "coderbyte"
+// Output: bcdeeorty
+// Input: "hooplah"
+// Output: ahhloop
+
+function AlphabetSoup(str) { 
+  let alphabetized = str.split('').sort().join('')
+  
+  return alphabetized; 
+
+}
+
+console.log(AlphabetSoup('hello dolly'))
