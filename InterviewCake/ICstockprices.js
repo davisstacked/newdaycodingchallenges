5b51003d36287c669196b6f42b0212e34b188bd1
davisstacked <brettzvi@gmail.com>
2021-11-02 14:05:12 -0700

    oneloopsolution for stockprices

--- InterviewCake/ICstockprices.js
+++ InterviewCake/ICstockprices.js
@@ -42,3 +42,24 @@ const getMaxProfit = () => {
 }
 
 //  O(n^2) runtime for the above solution.
+
+// Better Solution
+function getMaxProfitBetter(stockPrices) {
+  let minPrice = stockPrices[0];
+  let maxProfit = 0;
+
+  for (let i = 0; i < stockPrices.length; i++) {
+    const currentPrice = stockPrices[i];
+
+    // Ensure minPrice is the lowest price we've seen so far
+    minPrice = Math.min(minPrice, currentPrice);
+
+    // See what our profit would be if we bought at the min price and sold at the current price
+    const potentialProfit = currentPrice - minPrice;
+
+    // Update maxProfit if we can do better
+    maxProfit = Math.max(maxProfit, potentialProfit)
+  }
+
+  return maxProfit;
+}
