f573ac7592d1cb1f381d8b4c55ca70e703271669
davisstacked <brettzvi@gmail.com>
2021-11-11 15:14:24 -0800

    DFS preOrder function added to binary Search Tree class

--- InterviewCake/TreesandGraphs/binarysearchtree.js
+++ InterviewCake/TreesandGraphs/binarysearchtree.js
@@ -4,6 +4,14 @@
 // insertion - O(log n)
 // searching - O(log n)
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  };
+};
+
 class BinarySearchTree {
   constructor() {
     this.root = null;
@@ -70,14 +78,23 @@ class BinarySearchTree {
       if (node.right) queue.push(node.right);
     }
     return visitedValues;
-}
+  };
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+  DFSPreOrder = () => {
+    const data = [];
+    const current = this.root;
+
+    const traverse = (node) => {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+
+    return data;
   }
+
 }
 
 const tree = new BinarySearchTree();
