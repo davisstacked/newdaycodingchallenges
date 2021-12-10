d22a1dff774127924947cee31036c24f10b54c8b
davisstacked <brettzvi@gmail.com>
2021-12-09 16:34:39 -0800

    Frequency Counter solution

---
+++ CheatSheet.js
@@ -0,0 +1,17 @@
+// FOR ... IN LOOP 
+// ITERATES OVER THE KEYS OF AN OBJECT
+
+// FOR ... OF LOOP
+// ITERATES OVER THE VALUES OF AN OBJECT OR AN ARRAY 
+
+// obj.hasOwnProperty()
+
+// ** 	EXPONATION
+// 2**2 is 2^2. 
+
+
+// SLICE
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+
+// SPLICE
+// The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. To access part of an array without modifying it, see slice().
