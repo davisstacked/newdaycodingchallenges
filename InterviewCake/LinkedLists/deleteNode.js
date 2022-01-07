b29343e6c038f205cd30ca0c0184c6f23c7847dd
davisstacked <brettzvi@gmail.com>
2022-01-07 12:05:17 -0800

    delete node

---
+++ InterviewCake/LinkedLists/deleteNode.js
@@ -0,0 +1,35 @@
+// Delete a node from a singly-linked list, ↴ given only a variable pointing to that node.
+
+// The input could, for example, be the variable b below:
+
+  class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const a = new LinkedListNode('A');
+const b = new LinkedListNode('B');
+const c = new LinkedListNode('C');
+
+a.next = b;
+b.next = c;
+
+deleteNode(b);
+
+function deleteNode(nodeToDelete) {
+  // Get the input node's next node, the one we want to skip to
+  const nextNode = nodeToDelete.next;
+
+  if (nextNode) {
+    // Replace the input node's value and pointer with the next
+    // node's value and pointer. The previous node now effectively
+    // skips over the input node. 
+    nodeToDelete.value = nextNode.value;
+    nodeToDelete.next = nextNode.next;
+  } else {
+    // Eep, we're trying to delete the last node!
+    throw new Error("Can't delete the last node with this technique!");
+  }
+}
