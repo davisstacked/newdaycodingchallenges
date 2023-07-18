e389d6208af302534feb6b221935a10d5cade927
davisstacked <brettzvi@gmail.com>
2023-07-18 13:08:19 -0700

    DFS preorder

---
+++ Udemy/DFSPreOrderTree.js
@@ -0,0 +1,15 @@
+// tree traversal
+// DFS - preorder - first node, then all the lefts to the bottom, then the rights from the bottom
+const DFS = (node) => {
+  const visited = [];
+  let current = node; 
+  
+  const traverse = (currentNode) => {
+    visited.push(currentNode.value);
+    if (currentNode.left) traverse(currentNode.left)
+    if (currentNode.right) traverse(currentNode.right)
+    }
+  }
+  traverse(current);
+  return visited;
+}
