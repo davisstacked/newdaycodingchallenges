6b5a915a7fc464cf15f6f027bc61275854f39386
davisstacked <brettzvi@gmail.com>
2023-07-18 14:17:16 -0700

    DFS post order tree traversal

---
+++ Udemy/DFSPostOrderTree.js
@@ -0,0 +1,16 @@
+// tree traversal
+// DFS - postorder 
+// in postorder the root node is the last one visited/recorded. 
+const DFS = (node) => {
+  const visited = [];
+  let current = node; 
+  
+  const traverse = (currentNode) => {
+    if (currentNode.left) traverse(currentNode.left)
+    if (currentNode.right) traverse(currentNode.right)
+    visited.push(currentNode.value)
+    }
+  }
+  traverse(current);
+  return visited;
+}
